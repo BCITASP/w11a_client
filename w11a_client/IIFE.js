2d4ba7f5fe77f9392fbff0d663070b539a308627
alan_lai <alan@ergostratus.com>
2015-03-27 00:58:02 -0700

    Minor changes to use routes

--- w11a_client/IIFE.js
+++ w11a_client/IIFE.js
@@ -1,3 +1,18 @@
 ﻿(function () {
-    $scope.calc = 67 / 5;
+    var app = angular.module(["ngRoute"]);
+
+    app.config(function ($routeProvider) {
+        $routeProvider
+          .when("/main", {
+              templateUrl: "Default.html",
+              controller: "DefaultController"
+          })
+          .when("/user/:username", {
+              templateUrl: "user.html",
+              controller: "UserController"
+          })
+          .otherwise({
+              redirectTo: "/main"
+          });
+    });
 }());
